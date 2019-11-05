c4adb123b75ff1b72626158514c36f6e3f94d5f2
Marlene Silva Marchena <13339773+msmarchena@users.noreply.github.com>
2019-11-05 21:12:20 +0100

    Add files via upload

---
+++ IntPython_Variables.py
@@ -0,0 +1,29 @@
+##########################
+#      Variables         #
+#Auteur: Marlene Marchena#
+##########################
+
+text = "Hello world" #string
+nombre1 = 38  #integer
+decimal = 20.76 # float
+imaginaire = 5 + 2j # imaginary
+boolean = True  # (or False) Boolean
+
+print(text)
+print(nombre1)
+print(decimal)
+print(imaginaire)
+print(boolean)
+
+#on peut changer la valeur de notre variable
+message = "Hello there"
+print(message)
+
+message = "Bonjour"
+print(message)
+
+#On peut verifier le type de variable avec la funtion type()
+
+#Exercices
+#1.Creer la variable prenom avec votre prénome
+#2. Faire un print de la variable prenom
