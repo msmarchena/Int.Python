076d0e2841614fb052c32cb05fb96dafbe4c13ea
Marlene Silva Marchena <13339773+msmarchena@users.noreply.github.com>
2019-11-13 12:58:57 +0100

    Add files via upload

---
+++ IntPython_Strings_1.py
@@ -0,0 +1,44 @@
+##########################
+#Strings primière partie #
+#Auteur: Marlene Marchena#
+##########################
+message = "Bonjour tout le monde"
+print(message)
+
+#utiliser "" ou '' ?
+#Dans ce cas est indiferent
+texte1 = "Je suis Marlene"
+texte2 = 'Je suis Marlene'
+print(texte1)
+print(texte2)
+#mais dans ce cas on doit utiliser ""
+texte3 = "Je m'appelle Marlene"
+print(texte3)
+#texte3 = 'Je m'appelle Marlene' #donne erreur
+texte3 = 'Je m\'appelle Marlene'
+
+message = "Bonjour tout le monde"
+print(message)
+
+texte4 = "Bonjour"
+texte5 = 'tout le monde'
+
+print(texte4 + " " + texte5)
+########################
+#commentaires in python#
+########################
+# on utilise "#" pour commentaire une ligne. Comment dans cette lingne !
+
+#On utilise """ """ pour fair de commentaire avec plusieur lignes
+"""
+pour fair de
+comentaire avec
+plusieur lignes
+"""
+#Exercices
+#1. Print les textes suivant
+# Hello world
+# Aujourd'hui
+#C'est "Dommage !"
+#"je t’aime" dit-elle
+#le fichier à importer est Z:\codage\file.txt
