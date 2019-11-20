289dd99a02353ebe35b56481841833ce5f2b3f6f
Marlene Silva Marchena <13339773+msmarchena@users.noreply.github.com>
2019-11-20 10:20:01 +0100

    Add files via upload

---
+++ IntPython_Strings_2.py
@@ -0,0 +1,34 @@
+##########################
+#Strings deuxième partie #
+#Auteur: Marlene Marchena#
+##########################
+message = "Bonjour tout le monde"
+print(message)
+
+text1 = "Bonjour"
+text2 = 'tout le monde'
+
+print(text1 + " " + text2)
+
+#on peut faire des operation avec strings
+print(text1 * 3)
+
+print(len(message))
+# print(message.title())
+# print(message.upper()) #convert la chaîne en lettres capital
+# print(message.lower())
+# print(message.find("j")) #cherche la position de la lettre j
+# print(message.count("o")) #donne le nombre de fois que "o" aparait in la chaîne
+#
+# #Ne oublie pas de creer une nouvelle variable
+# new_message = message.replace("o", "M")
+# print(new_message)
+# message_sep =  message.split(" ")
+# print(message_sep)
+
+
+#1. Créer une variable mot avec le string banane
+#Créer une nouvelle variable mot1 avec le string "banane" en lettes capitales, i.e,
+# "BANANE", utiliser la function upper
+
+#2. Changer la lettre 'n' du mot banane par '$', utiliser la function replace
