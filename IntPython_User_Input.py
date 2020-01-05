7862d328b79e56d2cdfc9fe3525601d050ac0fdd
Marlene Silva Marchena <13339773+msmarchena@users.noreply.github.com>
2020-01-05 20:29:38 +0100

    Add files via upload

---
+++ IntPython_User_Input.py
@@ -0,0 +1,18 @@
+
+prenom = 'Jules'
+
+age = 5
+
+print("Ton prénom est:", prenom)
+
+print("Ton âge est:", age)
+
+#On va demander au utilisateur son prénom et son âge
+
+prenom_utilisateur = input("Comment tu t'appelle ?")
+
+age_utilisateur = input("Quel âge as-tu ?")
+
+print("Ton prénom est:", prenom_utilisateur)
+
+print("Ton âge est:", age_utilisateur)
